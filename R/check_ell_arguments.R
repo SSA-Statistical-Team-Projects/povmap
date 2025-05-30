dafa6190da22e59fba1a126daa8140c95a06807d
David Newhouse <david.newhouse@gmail.com>
2025-05-30 11:34:49 -0400

    Update check_ell_arguments.R

--- R/check_ell_arguments.R
+++ R/check_ell_arguments.R
@@ -46,8 +46,7 @@ ell_check1 <- function(fixed, pop_data, pop_domains, smp_data, smp_domains, L,tr
                 population data."))
   }
   
-  y <- as.character(fixed[[2]])
-  if (transformation=="arcsin" & (min(smp_data[,y],na.rm=T)<0 | max(smp_data[,y],na.rm=T)>1))  {
+  if (transformation=="arcsin" & (min(smp_data[,as.character(fixed[[2]])],na.rm=T)<0 | max(smp_data[,as.character(fixed[[2]])],na.rm=T)>1))  {
     stop("The dependent variable must be between zero and one when using the arcsin transformation")
   }
   
